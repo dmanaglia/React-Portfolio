c2b7055485d18113cf9b2cee6db331decf8883e4
dmanaglia <ddsmm.managlia@gmail.com>
2023-03-29 14:42:39 -0500

    updated contact page

--- src/components/Main/Contact/index.js
+++ src/components/Main/Contact/index.js
@@ -2,89 +2,86 @@ import React, { useState } from 'react';
 import './style.css';
 
 // Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from './../../../utils/helpers';
+import { validateEmail } from './../../../utils/helpers';
 
 function Contact() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
   const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitMessage, setSubmitMessage] = useState('');
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
+  const handleInputChange = ({ target }) => {
     const inputType = target.name;
     const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
       setUserName(inputValue);
     } else {
-      setPassword(inputValue);
+      setMessage(inputValue);
     }
   };
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
 
     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
+    if (!validateEmail(email)) {
+      setSubmitMessage(`${email} is not a valid email.`);
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password for the account: ${userName}`
+    } else if (!message) {
+      setSubmitMessage(
+        `Message cannot be empty`
+      );
+      return;
+    } else if (!userName){
+      setSubmitMessage(
+        `Name cannot be empty`
       );
       return;
     }
-    alert(`Hello ${userName}`);
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    //save here? save at all?
+    setSubmitMessage(`Contact Request Submited!`);
     setUserName('');
-    setPassword('');
+    setMessage('');
     setEmail('');
   };
 
   return (
     <div>
-      <p>Hello {userName}</p>
+      <h2 className='pageHeader'>Contact</h2>
       <form className="form">
         <input
           value={email}
           name="email"
           onChange={handleInputChange}
           type="email"
-          placeholder="email"
-        />
+          placeholder='Email:'
+          />
         <input
           value={userName}
           name="userName"
           onChange={handleInputChange}
           type="text"
-          placeholder="username"
+          placeholder='Name:'
         />
-        <input
-          value={password}
-          name="password"
+        <textarea
+          value={message}
+          name="message"
           onChange={handleInputChange}
-          type="password"
-          placeholder="Password"
+          placeholder="Message"
+          rows="7"
+          cols="60"
         />
+        {submitMessage && (
+          <div>
+            <p className="submit-text">{submitMessage}</p>
+          </div>
+        )}
         <button type="button" className='btn btn-primary' onClick={handleFormSubmit}>Submit</button>
       </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
     </div>
   );
 }
